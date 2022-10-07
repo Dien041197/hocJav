58a3881605ad223d12905b0061727748439e1b8d
Dien041197 <115073485+Dien041197@users.noreply.github.com>
2022-10-07 17:31:00 +0700

    nua bai 7

--- src/buoi2/bai7.java
+++ src/buoi2/bai7.java
@@ -1,4 +1,23 @@
 package buoi2;
 
+import java.util.Scanner;
+
 public class bai7 {
+    public static void main(String[] args) {
+        int h,m,s;
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Nhap gio: ");
+        h=sc.nextInt();
+        System.out.print("Nhap phut: ");
+        m=sc.nextInt();
+        System.out.print("Nhap giay: ");
+        s=sc.nextInt();
+        if(h>=0&&h<24||m>=0&&m<60||s>=60&&s<60){
+            System.out.print(h+":"+m+":"+s );
+            System.out.println("\n");
+            System.out.print(h+":"+m+":"+(s-1) );
+            System.out.println("\n");
+            System.out.print(h+":"+m+":"+(s+1) );
+        }
+    }
 }
