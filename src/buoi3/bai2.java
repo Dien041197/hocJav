a40d086d7d8c8ba00989c5ce0b159d70c2d07fef
Dien041197 <115073485+Dien041197@users.noreply.github.com>
2022-10-11 20:01:29 +0700

    update bai 2

--- src/buoi3/bai2.java
+++ src/buoi3/bai2.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class bai2 {
     public static void main(String[] args) {
         int n=0;
-        int giaithua =1;
-        int tichgiaithua =0;
+        int giaiThua =1;
+        int tichGiaiThua =0;
         Scanner sc=new Scanner(System.in);
         System.out.print("nhap n: ");
         n=sc.nextInt();
         for (int i=1;i<=n;i++){
-            giaithua = giaithua*i;
-            tichgiaithua = tichgiaithua+giaithua;
+            giaiThua = giaiThua*i;
+            tichGiaiThua = tichGiaiThua+giaiThua;
         }
-        System.out.print("gia thua: "+giaithua);
-        System.out.print("\ntich giai thua: "+tichgiaithua);
+        System.out.print("gia thua: "+giaiThua);
+        System.out.print("\ntich giai thua: "+tichGiaiThua);
         System.out.print("\ncac so chia het cho 3 hoac 7 la: ");
         for (int i=1;i<=n;i++){
             if(i%3==0||i%7==0){
