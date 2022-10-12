c744c589d67e3891644d8d02385d4e6b859d7fad
Dien041197 <115073485+Dien041197@users.noreply.github.com>
2022-10-12 17:15:54 +0700

    update bai 1 buoi 4

--- src/buoi4/bai1.java
+++ src/buoi4/bai1.java
@@ -2,22 +2,46 @@ package buoi4;
 
 public class bai1 {
     public static void main(String[] args) {
-        int arr[] =  { 10, 25, -4, 32, 63, 81, 19,-24, 13, 18, 45, 12, 72, 42, -6 };
-        int tong=0;
-        for (int i = 0; i <= arr.length; i++) {
+        int arr[] = {10, 25,-4, 32, 63, 81, 19, -24, 13, 18, 45, 12, 72, 42,-6};
+        int sum = 0;
+        System.out.print("Yêu cầu 1:");
+        for (int i=0;i<arr.length;i++){
             if(arr[i]%2!=0) {
-                System.out.println(i + ":" + (arr[i]));
+                System.out.print("\n-arr["+i + "]:" + arr[i]);
+                sum=sum+arr[i];
             }
         }
-        System.out.println("\n");
-        for (int i = 0; i <= arr.length; i++) {
+
+        System.out.print("\nYêu cầu 2:");
+        for (int i=0;i<arr.length;i++){
             if(arr[i]%2==0) {
                 if(arr[i]%6==0) {
-                    System.out.println(i + ":" + (arr[i]));
+                    System.out.print("\n-arr[" + i + "]:" + arr[i]);
                 }
             }
         }
-        
+
+        System.out.print("\nYêu cầu 3:");
+        System.out.print("\nTổng chẵn là: "+sum);
+
+        System.out.print("\nYêu cầu 4:");
+        System.out.print("\n-arr[]={");
+        for (int i=0;i<arr.length;i++){
+            if(i==args.length-1) {
+                if(arr[i]<0) {
+                    arr[i] = 0;
+                    System.out.print(arr[i] + "}");
+                }else {
+                    arr[i] = 0;
+                    System.out.print(arr[i] + "}");
+                }
+            }else if(arr[i]<0) {
+                arr[i] = 0;
+                System.out.print(arr[i] + ",");
+            }else {
+                System.out.print(arr[i] + ",");
+            }
+        }
 
     }
 }
