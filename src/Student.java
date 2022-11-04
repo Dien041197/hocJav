8ef21daecbb3a3e0ef5be36fe133f04a0da95c97
Dien041197 <115073485+Dien041197@users.noreply.github.com>
2022-11-04 08:10:58 +0700

    up

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String id;
+    String name;
+    int age;
+    int point;
+    public Student(String id, String name, int age, int point){
+        this.id=id;
+        this.name=name;
+    }
+}
